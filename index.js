2bb43279104bc832f455fe06adb42aa36ce1b4d0
Abhishek Panwar <imabhishek612@gmail.com>
2023-02-15 04:34:21 +0530

    playing around with server

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const Person = require("./person.js");
-const person1 = new Person("Abhishek", 23);
-person1.greeting();
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("<h1>Home</h1>");
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
