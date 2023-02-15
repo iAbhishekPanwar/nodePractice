14c9e1f6943dcdd0f37fdfe74dc7ab4bd4f59f89
Abhishek Panwar <imabhishek612@gmail.com>
2023-02-15 13:42:19 +0530

    running local server with nodemon

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.end("<h1>home</h1>");
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+
+  if (req.url === "/api/users") {
+    const users = [
+      { name: "Bob Smith", age: 40 },
+      { name: "Tom Hanks", age: 53 },
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
   }
 });
 
